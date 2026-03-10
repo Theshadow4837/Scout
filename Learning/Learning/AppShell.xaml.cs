ca19fedf3cad7633c8b4f75c64786c9fee7510ce
Janik Dhadwar <Janikdhadwar23@gmail.com>
2026-03-10 15:44:41 -0700

    m

---
+++ Learning/Learning/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Learning
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Learning/Learning/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Learning
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
